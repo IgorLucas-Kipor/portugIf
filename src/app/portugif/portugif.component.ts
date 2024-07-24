0062a88941d7235d065d63e197c5d83d3a3b85dd
IgorLucas-Kipor <igorescritor@hotmail.com>
2024-07-24 01:01:03 -0300

    - Correções de erros de CSS;

--- src/app/portugif/portugif.component.ts
+++ src/app/portugif/portugif.component.ts
@@ -12,6 +12,7 @@ export class PortugifComponent implements OnInit {
   firstConditionTranslatedCode: string = '';
   secondConditionTranslatedCode: string = '';
   printTranslatedCode: string = '';
+  closeCode: string = '}';
   generatedCode = false;
 
   textToCodeOutput = this.fb.group({
@@ -67,14 +68,12 @@ export class PortugifComponent implements OnInit {
 
   translateCode() {
 
-    this.firstConditionTranslatedCode = `${this.textToCodeOutput.get('textStart')?.value.toLowerCase()} ${this.textToCodeOutput.get('insertConnector')?.value ? '(' : ''}${this.textToCodeOutput.get('firstNegative')?.value ? '(!' : '('}${this.textToCodeOutput.get('firstFreeInput')?.value +')'}${this.textToCodeOutput.get('insertConnector')?.value ? '' : '{'}`;
+    this.firstConditionTranslatedCode = `${this.textToCodeOutput.get('textStart')?.value.toLowerCase()} ${this.textToCodeOutput.get('insertConnector')?.value ? '(' : ''}${this.textToCodeOutput.get('firstNegative')?.value ? '(!' : '('}${this.textToCodeOutput.get('firstFreeInput')?.value +') '}${this.textToCodeOutput.get('insertConnector')?.value ? '' : '{'}`;
     if (this.textToCodeOutput.get('insertConnector')?.value) {
       this.secondConditionTranslatedCode = ` ${this.textToCodeOutput.get('textConnector')?.value} ${this.textToCodeOutput.get('secondNegative')?.value ? '(!' : '('}${this.textToCodeOutput.get('secondFreeInput')?.value + ')'}) {`
     }
 
-    this.printTranslatedCode += `     escreva("${this.textToCodeOutput.get('thirdNegative')?.value ? 'não ' : ''}${this.textToCodeOutput.get('thirdFreeInput')?.value})
-
-              }`;
+    this.printTranslatedCode += ` escreva("${this.textToCodeOutput.get('thirdNegative')?.value ? 'não ' : ''}${this.textToCodeOutput.get('thirdFreeInput')?.value}")`;
 
     this.generatedCode = true;
 
